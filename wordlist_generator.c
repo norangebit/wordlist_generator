541d7ed392e0905a31b364845d3f5420631f01c8
OrangeDugongo <raffy.m.96@gmail.com>
2016-04-08 19:30:20 +0200

    fix

--- wordlist_generator.c
+++ wordlist_generator.c
@@ -24,7 +24,7 @@ int main(){
 
   //INSERIMENTO VALORI SUL FILE
   if((string=fopen("string", "w"))){
-    printf("Inserisci le informazioni sul sogetto: ");
+    printf("Inserisci informazioni(max %d) sul target separate da spazi.\nQuando hai terminato inserisci il cancelletto '#' e premi invio\nEsempio: franco rossi 1990 milan roberta #\n", INFO);
     do{
       fputc((c=getchar()), string);
     }while(c!=35);
@@ -45,37 +45,37 @@ int main(){
   diz = fopen("dizionario", "w");
   //INIZIO CICLO
   for(i=0;i<mixa;i++){
-  	
+
     //SECONDO CICLO
     for(j=0;j<mixa;j++){
       if(i==j)
         continue;
       fprintf(diz, "%s%s\n", info[i].str, info[j].str);
-      
+
       //TERZO CICLO
       for(h=0;h<mixa;h++){
         if(i==h || j==h || i==h)
           continue;
         fprintf(diz, "%s%s%s\n", info[i].str, info[j].str, info[h].str);
-        
+
         //QUARTO CICLO
         for(r=0;r<mixa;r++){
           if(i==h || j==h || i==h || r==i || r==h || r==j)
             continue;
           fprintf(diz, "%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str);
-          
+
           //QUINTO CICLO
           for(g=0;g<mixa;g++){
             if(i==h || j==h || i==h || r==i || r==h || r==j || g==i || g==j || g==h || g==r)
               continue;
             fprintf(diz, "%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str);
-            
+
             //SESTO CICLO
             for(f=0;f<mixa;f++){
               if(i==h || j==h || i==h || r==i || r==h || r==j || g==i || g==j || g==h || g==r || f==i || f==j || f==h || f==g || f==r)
                 continue;
               fprintf(diz, "%s%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str, info[f].str);
-              
+
               //SETTIMO CICLO
               for(e=0;e<mixa;e++){
                 if(i==h || j==h || i==h || r==i || r==h || r==j || g==i || g==j || g==h || g==r || f==i || f==j || f==h || f==g || f==r || e==i || e==j || e==g || e==h || e==r || e==f)
@@ -89,6 +89,6 @@ int main(){
     }
   }
 fclose(diz);
-printf("Dizionario creato\n");
+printf("dizionario creato con successo, il nome del dizionario è dizionario.");
 return(0);
 }
