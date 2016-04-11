37732778483d36e5e73c5ec12534828e9dfe92b6
OrangeDugongo <raffy.m.96@gmail.com>
2016-04-11 18:26:57 +0200

    upper

--- wordlist_generator.c
+++ wordlist_generator.c
@@ -2,13 +2,14 @@
  ============================================================================
  Name        : GeneratoreDizionari.c
  Author      : Sh13n, Orange_dugongo
- Version     :
- Copyright   : OpenSource
- Description : Generatore di dizionari in C, alpha 0.1.1
+ Version     : 
+ Copyright   :
+ Description : Generatore di dizionari in C
  ============================================================================
  */
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #define INFO 10
 
 typedef struct{
@@ -16,17 +17,17 @@ typedef struct{
 } String;
 
 int main(){
-  char c;
-  String info[INFO];
+  char c, s[]="Dizionario";
+  String info[2*INFO];
   FILE *string, *diz;
   int mixa=0, i, j, h, r, g, f, e;
 
 
   //INSERIMENTO VALORI SUL FILE
   if((string=fopen("string", "w"))){
-    printf("Inserisci le informazioni sul sogetto: ");
+    printf("Inserisci informazioni(max %d) sul target separate da spazi.\nQuando hai terminato inserisci il cancelletto '#' e premi invio\nEsempio: franco rossi 1990 milan roberta #\n", INFO);
     do{
-      fputc((c=getchar()), string);
+      fputc(tolower((c=getchar())), string);
     }while(c!=35);
     fclose(string);
   }else{
@@ -38,47 +39,52 @@ int main(){
   string=fopen("string", "r");
   do{
     fscanf(string, "%s", info[mixa].str);
+    //se il primo carattere della stringa è alfabetico genero la stringa maiuscola
+    if(isalpha(info[mixa].str[0])){
+      info[mixa+1]=info[mixa++];
+      info[mixa].str[0]=toupper(info[mixa].str[0]);
+    }
   }while (strcmp("#", info[mixa].str) && mixa++<INFO);
   fclose(string);
 
   //APERTURA FILE
-  diz = fopen("dizionario", "w");
+  diz = fopen(s, "w");
   //INIZIO CICLO
   for(i=0;i<mixa;i++){
 
     //SECONDO CICLO
     for(j=0;j<mixa;j++){
-      if(i==j)
+      if(!strcasecmp(info[i].str, info[j].str))
         continue;
       fprintf(diz, "%s%s\n", info[i].str, info[j].str);
 
       //TERZO CICLO
       for(h=0;h<mixa;h++){
-        if(i==h || j==h || i==h)
+        if(!strcasecmp(info[i].str, info[h].str) || !strcasecmp(info[j].str, info[h].str))
           continue;
         fprintf(diz, "%s%s%s\n", info[i].str, info[j].str, info[h].str);
 
         //QUARTO CICLO
         for(r=0;r<mixa;r++){
-          if(i==h || j==h || i==h || r==i || r==h || r==j)
+          if(!strcasecmp(info[i].str, info[r].str) || !strcasecmp(info[j].str, info[r].str) || !strcasecmp(info[h].str, info[r].str))
             continue;
           fprintf(diz, "%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str);
 
           //QUINTO CICLO
           for(g=0;g<mixa;g++){
-            if(i==h || j==h || i==h || r==i || r==h || r==j || g==i || g==j || g==h || g==r)
+            if(!strcasecmp(info[i].str, info[g].str) || !strcasecmp(info[j].str, info[g].str) || !strcasecmp(info[h].str, info[g].str) || !strcasecmp(info[r].str, info[g].str))
               continue;
             fprintf(diz, "%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str);
 
             //SESTO CICLO
             for(f=0;f<mixa;f++){
-              if(i==h || j==h || i==h || r==i || r==h || r==j || g==i || g==j || g==h || g==r || f==i || f==j || f==h || f==g || f==r)
+              if(!strcasecmp(info[i].str, info[f].str) || !strcasecmp(info[j].str, info[f].str) || !strcasecmp(info[h].str, info[f].str) || !strcasecmp(info[r].str, info[f].str) || !strcasecmp(info[g].str, info[f].str))
                 continue;
               fprintf(diz, "%s%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str, info[f].str);
 
               //SETTIMO CICLO
               for(e=0;e<mixa;e++){
-                if(i==h || j==h || i==h || r==i || r==h || r==j || g==i || g==j || g==h || g==r || f==i || f==j || f==h || f==g || f==r || e==i || e==j || e==g || e==h || e==r || e==f)
+                if(!strcasecmp(info[i].str, info[e].str) || !strcasecmp(info[j].str, info[e].str) || !strcasecmp(info[h].str, info[e].str) || !strcasecmp(info[r].str, info[e].str) || !strcasecmp(info[g].str, info[e].str) || !strcasecmp(info[f].str, info[e].str))
                   continue;
                 fprintf(diz, "%s%s%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str, info[f].str, info[e].str);
               }
@@ -89,6 +95,6 @@ int main(){
     }
   }
   fclose(diz);
-  printf("Dizionario creato\n");
+  printf("dizionario creato con successo, il nome del dizionario è %s.", s);
   return(0);
 }
