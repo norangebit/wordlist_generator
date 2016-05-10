6c39edadb7e47cc162f968a95592d7f66586565f
OrangeDugongo <raffy.m.96@gmail.com>
2016-05-10 21:50:00 +0200

    Loading from file

--- wordlist_generator.c
+++ wordlist_generator.c
@@ -1,10 +1,10 @@
 /*
 ============================================================================
-Name        : GeneratoreDizionari.c
+Name        : Generatoredestionari.c
 Author      : Sh13n, Orange_dugongo
 Version     : alpha 0.3
 Copyright   : CC BY-SA 3.0 IT
-Description : Generatore di dizionari in C
+Description : Generatore di destionari in C
 ============================================================================
  */
 #include <stdio.h>
@@ -15,45 +15,48 @@ Description : Generatore di dizionari in C
 
 typedef struct{
   char *str;
-} String;
+} src;
 
 int main(){
-  char c, s[]="Dizionario", buffer[200];
-  String info[2*INFO];
-  FILE *string, *diz;
+  char c, s[]="destionario", buffer[200];
+  src info[2*INFO];
+  FILE *src, *dest;
   int mixa=0, i, j, h, r, g, f, e;
 
-
+  printf("Da quale file vuoi caricare i dati: ");
+  scanf("%s", buffer);
   //INSERIMENTO VALORI SUL FILE
-  if((string=fopen("string", "w"))){
-    printf("Inserisci informazioni(max %d) sul target separate da spazi.\nQuando hai terminato inserisci il cancelletto '#' e premi invio\nEsempio: franco rossi 1990 milan roberta #\n", INFO);
-    do{
-      fputc(tolower((c=getchar())), string);
-    }while(c!=35);
-    fclose(string);
-  }else{
-    printf("Si è verificato un errore.");
-    return(0);
+  if(!(src=fopen(buffer, "r"))){
+    printf("Il file indicato non esiste, ora verrà creato\n");
+    if((src=fopen(buffer, "w"))){
+      printf("Inserisci informazioni(max %d) sul target separate da spazi.\nQuando hai terminato inserisci il cancelletto '#' e premi invio\nEsempio: franco rossi 1990 milan roberta #\n", INFO);
+      do{
+        fputc(tolower((c=getchar())), src);
+      }while(c!=35);
+    }else{
+      printf("Si è verificato un errore.");
+      return(0);
+    }
   }
-
+  fclose(src);
   //CONTROLLO SE L'UTENTE HA INSERITO VALORI NEGLI ARRAY
-  string=fopen("string", "r");
+  src=fopen(buffer, "r");
   do{
-    fscanf(string, "%s", buffer);
+    fscanf(src, "%s", buffer);
     info[mixa].str=(char *)calloc(strlen(buffer)+1, sizeof(char));
     strcpy(info[mixa].str, buffer);
 
-    //se il primo carattere della stringa è alfabetico genero la stringa maiuscola
+    //se il primo carattere della srca è alfabetico genero la srca maiuscola
     if(isalpha(info[mixa].str[0])){
       info[++mixa].str=(char *)calloc(strlen(buffer)+1, sizeof(char));
       buffer[0]=toupper(buffer[0]);
       strcpy(info[mixa].str, buffer);
     }
   }while (strcmp("#", info[mixa].str) && mixa++<INFO);
-  fclose(string);
+  fclose(src);
 
   //APERTURA FILE
-  diz = fopen(s, "w");
+  dest = fopen(s, "w");
   //INIZIO CICLO
   for(i=0;i<mixa;i++){
 
@@ -61,37 +64,37 @@ int main(){
     for(j=0;j<mixa;j++){
       if(!strcasecmp(info[i].str, info[j].str))
         continue;
-      fprintf(diz, "%s%s\n", info[i].str, info[j].str);
+      fprintf(dest, "%s%s\n", info[i].str, info[j].str);
 
       //TERZO CICLO
       for(h=0;h<mixa;h++){
         if(!strcasecmp(info[i].str, info[h].str) || !strcasecmp(info[j].str, info[h].str))
           continue;
-        fprintf(diz, "%s%s%s\n", info[i].str, info[j].str, info[h].str);
+        fprintf(dest, "%s%s%s\n", info[i].str, info[j].str, info[h].str);
 
         //QUARTO CICLO
         for(r=0;r<mixa;r++){
           if(!strcasecmp(info[i].str, info[r].str) || !strcasecmp(info[j].str, info[r].str) || !strcasecmp(info[h].str, info[r].str))
             continue;
-          fprintf(diz, "%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str);
+          fprintf(dest, "%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str);
 
           //QUINTO CICLO
           for(g=0;g<mixa;g++){
             if(!strcasecmp(info[i].str, info[g].str) || !strcasecmp(info[j].str, info[g].str) || !strcasecmp(info[h].str, info[g].str) || !strcasecmp(info[r].str, info[g].str))
               continue;
-            fprintf(diz, "%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str);
+            fprintf(dest, "%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str);
 
             //SESTO CICLO
             for(f=0;f<mixa;f++){
               if(!strcasecmp(info[i].str, info[f].str) || !strcasecmp(info[j].str, info[f].str) || !strcasecmp(info[h].str, info[f].str) || !strcasecmp(info[r].str, info[f].str) || !strcasecmp(info[g].str, info[f].str))
                 continue;
-              fprintf(diz, "%s%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str, info[f].str);
+              fprintf(dest, "%s%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str, info[f].str);
 
               //SETTIMO CICLO
               for(e=0;e<mixa;e++){
                 if(!strcasecmp(info[i].str, info[e].str) || !strcasecmp(info[j].str, info[e].str) || !strcasecmp(info[h].str, info[e].str) || !strcasecmp(info[r].str, info[e].str) || !strcasecmp(info[g].str, info[e].str) || !strcasecmp(info[f].str, info[e].str))
                   continue;
-                fprintf(diz, "%s%s%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str, info[f].str, info[e].str);
+                fprintf(dest, "%s%s%s%s%s%s%s\n", info[i].str, info[j].str, info[h].str, info[r].str, info[g].str, info[f].str, info[e].str);
               }
             }
           }
@@ -99,7 +102,7 @@ int main(){
       }
     }
   }
-  fclose(diz);
-  printf("dizionario creato con successo, il nome del dizionario è %s.", s);
+  fclose(dest);
+  printf("destionario creato con successo, il nome del destionario è %s.", s);
   return(0);
 }
