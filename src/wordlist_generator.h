44d448c080fe0b968d506d41b5ff1a7f6632c591
OrangeDugongo <raffy.m.96@gmail.com>
2016-05-15 12:26:57 +0200

    Add new functions

--- src/wordlist_generator.h
+++ src/wordlist_generator.h
@@ -22,37 +22,60 @@ This file is part of wordlist_generator.
     along with wordlist_generator.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
+
 /* IN QUESTO FILE SONO RIPORTATI SOLO I PROTOTIPI DELLE FUNZIONI E UNA BREVE
    SPIEGAZIONE DEL LORO COMPORTAMENTO. PER LEGGERE IL CODICE DELLE FUNZIONI SI
    DEVE LEGGERE IL FILE 'wordlist_generator.c'.
 */
 
-
-
 #include "wordlist_generator.c"
-
-
-
-//LEGGE LE INFO DA TASTIERA E LE SALVA SU UN FILE
-  /*  Prende in input una stringa, e crea un file di testo col nome della stringa.
-      Nel file di testo vengono copiate le informazione inserite dall'utente.
-      La funzione restituisce come output 1 se non ci sono stati errori e
-      0 in caso d'errore.
-  */
-int Inserisci(char *buffer);
-
-
-//LEGGE LE INFO DA UN FILE E LE CARICA IN MEMORIA
-  /*  La funzione prende in input il puntatore al file da cui deve leggere le
-      info. Carica in memoria le info sul soggetto e restituisce come output il
-      numero di info sul soggetto.
-  */
-int Load(FILE *src);
-
 
 //LEGGE LE INFO DA MEMORIA E GENERA IL DIZIONARIO
   /*  La funzione acetta come input il puntatore al file su cui deve salvare il
-      dizionario e il numero di parole inserite dall'utente. La funzione essendo
+      dizionario e il puntatore al soggetto da cui ricava le info. La funzione essendo
       di tipo void non restituisce nessun elemento.
   */
-void Generazione(FILE *dest, int mixa);
+void Generazione(FILE *dest, Soggetto *x);
+
+
+//CREA UN NUOVO SOGGETTO
+  /*  La funzione permette di caricare in memoria un nuovo soggetto, comprensivo
+      di nome cognome e le relative info. La funzione restituisce in output
+      il puntatore al nuovo sogetto.
+  */
+Soggetto *NewS();
+
+
+//INSERISCE IN ORDINE ALFABETICO UN NUOVO SOGGETTO NELLA LISTA
+  /*  La funzione accetta in imput il puntatore al nuovo soggetto e il puntatore
+      al puntatore(doppio puntatore) alla testa della sista. La funzione
+      permette di inserire in ordine alfabetico(cognome nome) il nuovo soggetto
+      nella lista. La funzione non acetta omonimi.
+  */
+void Link(Soggetto *New, Soggetto **head);
+
+
+//PERMETTE DI SALVARE SU FILE LA LISTA DEI SOGGETTI7
+  /*  La funzione accetta in input il puntatore alla testa della lista e il
+      nome del file su cui si desidera salvare.
+  */
+void Save(Soggetto *head, char *fname);
+
+
+//LEGGE DA FILE UNA LISTA DI SOGGETTI E LA CARICA IN MEMORIA
+  /*  La funzione accetta in input il puntatore al puntatore(doppio puntatore)
+      alla testa della lista e il nome del file da cui si vuole leggere.
+      La funzione restituisce 0 in caso di errori di lettura e 1 se il
+      procedimento è andato a buon fine.
+  */
+int Read(Soggetto **head, char *fname);
+
+
+//CERCA UN SOGGETTO NELLA LISTA
+  /*  La funzione accetta in input il puntatore alla testa della lista, il nome
+      e il cognome del soggetto. La funzione restituisce il puntatore al
+      soggetto ricercato. Nel caso in cui non si verifichi nessun match la
+      funzione restituisce NULL.
+  */
+Soggetto *Search(Soggetto *head, char *nome, char *cnome);
